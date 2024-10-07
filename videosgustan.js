2b79a2a5f01e3d3c73dd6ee5039d87bfabc62d88
Jjmdianez <116769095+Jjmdianez@users.noreply.github.com>
2024-10-07 15:31:11 +0200

    Add files via upload

---
+++ videosgustan.js
@@ -0,0 +1,12 @@
+const videos = [
+  {
+    "title": "¡EL KFC MÁS PELIGROSO del MUNDO! ⚠️ ft. PLEX 🇨🇳 Xokas en China",
+    "url": "https://www.youtube.com/watch?v=lrhv5s0ziUU",
+    "thumbnail": "https://img.youtube.com/vi/lrhv5s0ziUU/0.jpg"
+  },
+  {
+    "title": "Los nuevos TAPONES 😂🥤🚫 de las BOTELLAS",
+    "url": "https://www.youtube.com/watch?v=MJldgXqOOGk",
+    "thumbnail": "https://img.youtube.com/vi/MJldgXqOOGk/0.jpg"
+  }
+];
