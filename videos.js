def86bdfbb3d0e5b0a01428d4f363889445b211b
Jjmdianez <116769095+Jjmdianez@users.noreply.github.com>
2024-10-07 13:13:05 +0200

    Add files via upload

---
+++ videos.js
@@ -0,0 +1,12 @@
+const videos = [
+  {
+    "title": "La mamá y el hijo (parte 1)",
+    "url": "https://www.youtube.com/watch?v=d3Uro-Zeh-g",
+    "thumbnail": "https://img.youtube.com/vi/d3Uro-Zeh-g/0.jpg"
+  },
+  {
+    "title": "¡¡ANUNCIANDO AL GANADOR DEL SORTEO DEL TITAN CLOVER MAN!! (Toilet tower defense)",
+    "url": "https://www.youtube.com/watch?v=XDQJd-za1Sw",
+    "thumbnail": "https://img.youtube.com/vi/XDQJd-za1Sw/0.jpg"
+  }
+];
