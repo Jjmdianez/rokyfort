e559ed5e3bd836d74aa3e4727def7326a0b04be5
Jjmdianez <116769095+Jjmdianez@users.noreply.github.com>
2024-10-07 15:34:54 +0200

    Add files via upload

--- ultimosvideos.js
+++ ultimosvideos.js
@@ -1,7 +1,7 @@
 const videos = [
   {
     "title": "¡¡ANUNCIANDO AL GANADOR DEL SORTEO DEL TITAN CLOVER MAN!! (Toilet tower defense)",
-    "url": "https://www.youtube.com/watch?v=XDQJd-za1Sw&t=1s",
-    "thumbnail": "https://img.youtube.com/vi/XDQJd-za1Sw&t=1s/0.jpg"
+    "url": "https://www.youtube.com/watch?v=XDQJd-za1Sw",
+    "thumbnail": "https://img.youtube.com/vi/XDQJd-za1Sw/0.jpg"
   }
 ];
